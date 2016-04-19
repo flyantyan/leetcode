ce1d819d3b96b676609c359753889cca910e724e
flyyan <flyantyan@gmail.com>
2016-04-19 23:01:05 +0800

    Solved 1 problem 	E24 Swap Nodes in pairs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ##算法分类:
 	贪心算法:M12
+	
+	
+
+ListNode 使用技巧(避免判断)
+	ListNode result = new ListNode(0);
+	result.next = head ;
+	
+	return result.next ;
+	
+	
---
+++ src/leetcode/easy/E24_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+public class E24_Swap_Nodes_in_Pairs {
+
+	public ListNode swapPairs(ListNode head) {
+		ListNode result = new ListNode(0);
+		result.next = head;
+		ListNode current = result;
+
+		while (current.next != null && current.next.next != null) {
+			ListNode first = current.next;
+			ListNode second = current.next.next;
+
+			first.next = second.next;
+			current.next = second;
+			current.next.next = first;
+			current = current.next.next;
+		}
+
+		return result.next;
+	}
+}
